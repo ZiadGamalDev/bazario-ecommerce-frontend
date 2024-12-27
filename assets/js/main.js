423ae239e5d96a4193a4d494bf1c5386f80ac6fc
ZiadGamalDev <zyadgamal450@gmail.com>
2024-12-27 15:08:54 +0200

    initial commit: add project structure and core files for Glasses Store E-commerce

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function loadPartial(selector, filePath) {
+  const element = document.querySelector(selector);
+  if (element) {
+    fetch(filePath)
+      .then(response => {
+        if (!response.ok) throw new Error(`Failed to load ${filePath}`);
+        return response.text();
+      })
+      .then(html => {
+        element.innerHTML = html;
+      })
+      .catch(error => console.error(error));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadPartial("header", "pages/components/header.html");
+  loadPartial("nav", "pages/components/navbar.html");
+  loadPartial("footer", "pages/components/footer.html");
+});
