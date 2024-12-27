f9c0a7193edbf87d2f91edd4b441dc59bdc8263f
ZiadGamalDev <zyadgamal450@gmail.com>
2024-12-27 19:55:31 +0200

    Admin Category CRUD

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,9 @@
-function loadPartial(selector, filePath) {
+// const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'https://ecommerce.ershaad.net';
+const token = '1|mrVWnuPejI3DxDnAivijXlAHqizxXLcNFFus7ucqa374c833';
+const adminToken = '1|1AF5oYQQMPbetlaWGjKMdEIIolraXDeyZt2Eeh0s70da0d9c';
+
+function loadPartial(selector, filePath, callback) {
     const element = document.querySelector(selector);
     if (element) {
         fetch(filePath)
@@ -8,6 +13,7 @@ function loadPartial(selector, filePath) {
             })
             .then(html => {
                 element.innerHTML = html;
+                if (callback) callback();
             })
             .catch(error => console.error(error));
     }
