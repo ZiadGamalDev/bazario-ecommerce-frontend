da17b8c62a3338f7fcde36bfd7f36ea393a4d93d
sohailaabdelazeem <sohailaabdelazeem863@gmail.com>
2024-12-28 03:48:15 +0200

    products ()

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,11 @@
 // const baseUrl = 'http://127.0.0.1:8000';
+
 const baseUrl = 'https://ecommerce.ershaad.net';
-const token = '1|mrVWnuPejI3DxDnAivijXlAHqizxXLcNFFus7ucqa374c833';
-const adminToken = '1|1AF5oYQQMPbetlaWGjKMdEIIolraXDeyZt2Eeh0s70da0d9c';
+const token = 'customer-static-token';
+const adminToken = 'admin-static-token';
 
 function loadPartial(selector, filePath, callback) {
+<<<<<<< HEAD
     const element = document.querySelector(selector);
     if (element) {
         fetch(filePath)
@@ -19,3 +21,19 @@ function loadPartial(selector, filePath, callback) {
     }
 }
 
+=======
+  const element = document.querySelector(selector);
+  if (element) {
+    fetch(filePath)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Failed to load ${filePath}`);
+        return response.text();
+      })
+      .then((html) => {
+        element.innerHTML = html;
+        if (callback) callback();
+      })
+      .catch((error) => console.error(error));
+  }
+}
+>>>>>>> d5de0ecdc0df3b69bae4ff3f8c4880b7a4228796
