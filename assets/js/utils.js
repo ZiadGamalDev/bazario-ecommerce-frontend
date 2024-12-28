7c3231a1436b1ef6f812eb5e58b6f345e35900f3
3b3zeem <ahmedzezo2247@gmail.com>
2024-12-28 02:55:16 +0200

    fix about, contact, register and login

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,20 +1,20 @@
 // const baseUrl = 'http://127.0.0.1:8000';
-const baseUrl = 'https://ecommerce.ershaad.net';
-const token = '1|mrVWnuPejI3DxDnAivijXlAHqizxXLcNFFus7ucqa374c833';
-const adminToken = '1|1AF5oYQQMPbetlaWGjKMdEIIolraXDeyZt2Eeh0s70da0d9c';
+const baseUrl = "https://ecommerce.ershaad.net";
+const token = "customer-static-token";
+const adminToken = "admin-static-token";
 
 function loadPartial(selector, filePath, callback) {
-    const element = document.querySelector(selector);
-    if (element) {
-        fetch(filePath)
-            .then(response => {
-                if (!response.ok) throw new Error(`Failed to load ${filePath}`);
-                return response.text();
-            })
-            .then(html => {
-                element.innerHTML = html;
-                if (callback) callback();
-            })
-            .catch(error => console.error(error));
-    }
+  const element = document.querySelector(selector);
+  if (element) {
+    fetch(filePath)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Failed to load ${filePath}`);
+        return response.text();
+      })
+      .then((html) => {
+        element.innerHTML = html;
+        if (callback) callback();
+      })
+      .catch((error) => console.error(error));
+  }
 }
